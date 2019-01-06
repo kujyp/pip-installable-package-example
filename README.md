e687936d70d308f5c1821fd7077fbf877447e927
swe-jaeyoungpark <swe.jaeyoungpark@navercorp.com>
2019-01-06 18:35:53 +0900

    Add MANIFEST.in, setup.py

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 python app.py world
 python app.py name kujyp
 ```
+
+### after installation usage
+- installation
+```bash
+python setup.py sdist
+pip install dist/*.tar.gz
+```
+- usage
+```bash
+helloworld world
+helloworld name kujyp
+```
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+from helloworld import __version__, __description__
+
+
+def requirements():
+    with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as f:
+        ret = []
+        for eachline in f.readlines():
+            if eachline.startswith("-"):
+                continue
+            if eachline.startswith("git+") \
+                    or eachline.startswith("http://") \
+                    or eachline.startswith("https://"):
+                continue
+
+            ret.append(eachline)
+
+        return ret
+
+
+setup(name='helloworld',
+      version=__version__,
+      description=__description__,
+      packages=find_packages(exclude=[]),
+      entry_points="""
+        [console_scripts]
+        helloworld = helloworld.main:main
+      """,
+      include_package_data=True,
+      install_requires=requirements())
